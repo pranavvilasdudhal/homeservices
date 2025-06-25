36bd3e481969de86e67c2b4d0ef0b8ea1e5b79a4
= <=>
2025-06-25 14:47:11 +0530

    code8

---
+++ lib/login_screens/splashscreen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/screens/home_screen.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.miscellaneous_services,
+              size: 100,
+              color: Colors.blueAccent,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Services App",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueAccent,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const CircularProgressIndicator(
+              color: Colors.blueAccent,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/login_screens/splashscreen.dart';
 import 'screens/home_screen.dart';
-import 'screens/bookings_screen.dart';
-import 'screens/profile_screen.dart';
+
 
 void main() {
   runApp(HomeServicesApp());
 }
 
 class HomeServicesApp extends StatefulWidget {
+  const HomeServicesApp({super.key});
+
   @override
   _HomeServicesAppState createState() => _HomeServicesAppState();
 }
@@ -24,16 +26,7 @@ class _HomeServicesAppState extends State<HomeServicesApp> {
       title: 'Home Services App',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
-        body: _screens[_selectedIndex],
-        // bottomNavigationBar: BottomNavigationBar(
-        //   currentIndex: _selectedIndex,
-        //   onTap: (i) => setState(() => _selectedIndex = i),
-        //   items: const [
-        //     BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        //     BottomNavigationBarItem(icon: Icon(Icons.book), label: 'Bookings'),
-        //     BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        //   ],
-        // ),
+        body: const SplashScreen(),
       ),
     );
   }
